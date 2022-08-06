9070cb061bc929cf350bdd4807b4c2aa4fe6453e
Maksym Hanzha <101345604+MaksymHanzha@users.noreply.github.com>
2022-08-07 00:17:28 +0300

    --2_TASK--

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const wayToUl = document.querySelector("#ingredients");
+
+const list = ingredients.map((item) => {
+  const ingredient = document.createElement("li");
+  ingredient.textContent = item;
+  ingredient.class = "item";
+  return ingredient;
+});
+
+wayToUl.append(...list);
