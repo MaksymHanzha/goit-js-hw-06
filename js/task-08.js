255b3327faae210faa1cded7e447d7101a963f50
Maksym Hanzha <101345604+MaksymHanzha@users.noreply.github.com>
2022-08-08 17:17:28 +0300

    --8,9_TASK--

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", userSubmit);
+
+function userSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Заповніть форму!");
+  }
+
+  const user = {
+    Email: email.value,
+    Password: password.value,
+  };
+  console.log(user);
+
+  event.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector(".change-color");
+const colorValue = document.querySelector("span.color");
+
+btnChangeColor.addEventListener("click", () => {
+  let color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  colorValue.textContent = color;
+});
