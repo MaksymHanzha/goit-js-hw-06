ff4c05f57c177cd197ff5065c3814bd42ccbe2b6
Maksym Hanzha <101345604+MaksymHanzha@users.noreply.github.com>
2022-08-08 12:07:53 +0300

    --6_TASK--

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const input = document.querySelector("#validation-input");
+
+input.addEventListener("blur", valueBlur);
+
+function valueBlur(text) {
+  if (text.currentTarget.value.length === 0) {
+    input.classList.remove("valid");
+    input.classList.remove("invalid");
+  } else if (text.currentTarget.value.length !== Number(input.dataset.length)) {
+    input.classList.add("invalid");
+  } else {
+    input.classList.add("valid");
+  }
+}
